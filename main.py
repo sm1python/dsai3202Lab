94eba1750efe53254593e42ced209c2c192c6a89
sm1python <162344523+sm1python@users.noreply.github.com>
2025-02-09 10:04:31 +0300

    run performance analysis update

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import sys
 import os
 from src.threads import run_threads
 from src.multiprocessor import run_multiprocessing
+from src.preformance import *
 
 # Run threads and multiprocessing tasks
 if __name__ == "__main__":
     run_threads()
     run_multiprocessing()
+    performance_analysis()
