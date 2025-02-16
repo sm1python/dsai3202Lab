e596c016e085021d834ac509a27a1acf648f7aa9
sm1python <60103137@udst.edu.qa>
2025-02-16 10:17:17 +0300

    lab3 part 1 done

--- main.py
+++ main.py
@@ -10,22 +10,4 @@ Measures execution time for each approach and prints the results.
 """
 if __name__ == "__main__":
     n = 10**8
-    # start_time = time.time()
-    # seq_result = sequential_sum(n)
-    # seq_time = time.time() - start_time
-    # print(f"Sequential Sum from 1 to n: {seq_result}, Time: {seq_time:} sec")
-
-    # start_time = time.time()
-    # thread_result = threaded_sum(n)
-    # thread_time = time.time() - start_time
-    # print(f"Threaded Sum: {thread_result}, Time: {thread_time:.4f} sec")
-
-    # start_time = time.time()
-    # process_result = process_sum(n)
-    # process_time = time.time() - start_time
-    # print(f"Process Sum: {process_result}, Time: {process_time:.4f} sec")
-    
-    # print("Speedup (Threads):", seq_time / thread_time)
-    # print("Speedup (Processes):", seq_time / process_time)
     performance_analysis(n)
-# def run_all():
